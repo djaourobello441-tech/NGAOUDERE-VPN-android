369d17692f2ec80fa2533c3693df1478b049d5b2
djaourobello441-tech <djaourobello441@gmail.com>
2026-03-15 13:14:22 +0100

    Create VPN service.java

---
+++ VPN service.java
@@ -0,0 +1,15 @@
+String config = "vless://39382039-3720-4438-a039-392035332035@tr-1.stunnel.cc:8442?...";
+V2Ray.init(this);
+V2Ray.setConfig(config);
+V2Ray.start();
+// Ajoute V2Ray core
+import com.v2ray.ang.V2Ray;
+
+public class VpnService extends VpnService {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        V2Ray.init(this);
+        V2Ray.start();
+        // ...
+    }
+}
